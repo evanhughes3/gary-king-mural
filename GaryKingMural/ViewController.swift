7b41ada242c761d320b0bbef3c4750bc96c83fc3
Evan Hughes <evanhughes3@gmail.com>
2018-01-20 16:27:00 -0800

    Added project

---
+++ GaryKingMural/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GaryKingMural
+//
+//  Created by Evan Hughes on 1/20/18.
+//  Copyright © 2018 Evan Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
